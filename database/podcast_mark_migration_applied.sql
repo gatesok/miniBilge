13763a01c4e49882f5710e3b606f706cab5123fb
gatesok <gunesatesok@gmail.com>
2026-06-23 00:40:10 +0300

    chore: ignore database folder

--- database/podcast_mark_migration_applied.sql
+++
@@ -1,9 +0,0 @@
--- ============================================================
--- Migration kaydını EF history tablosuna ekle
--- Tabloları elle oluşturduktan sonra çalıştırın
--- Böylece MigrateAsync() bu migration'ı tekrar çalıştırmaz
--- ============================================================
-
-INSERT INTO "__EFMigrationsHistory_PostgreSQL" ("MigrationId", "ProductVersion")
-VALUES ('20260622120232_AddPodcastEntities', '8.0.0')
-ON CONFLICT ("MigrationId") DO NOTHING;
--- database/sprint19_user_online_speech.sql
+++
@@ -1,6 +0,0 @@
--- Sprint 19: Online speech permission flag
-ALTER TABLE users
-    ADD COLUMN IF NOT EXISTS "CanUseOnlineSpeech" BOOLEAN NOT NULL DEFAULT true;
-
--- Mevcut kullanıcıları true yap
-UPDATE users SET "CanUseOnlineSpeech" = true WHERE "CanUseOnlineSpeech" = false;
