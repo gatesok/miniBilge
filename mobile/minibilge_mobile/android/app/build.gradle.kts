e791c031f068a3b41a58ab0fb87926b8e941d8a0
gatesok <gunesatesok@gmail.com>
2026-07-22 13:40:17 +0300

    gradle version

--- mobile/minibilge_mobile/android/app/build.gradle.kts
+++ mobile/minibilge_mobile/android/app/build.gradle.kts
@@ -1,9 +1,19 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
+}
+
+// key.properties dosyasını okuma işlemi
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 android {
@@ -32,11 +42,21 @@ android {
         versionName = flutter.versionName
     }
 
+    // İmza ayarlarının oluşturulduğu blok
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String?
+            keyPassword = keystoreProperties["keyPassword"] as String?
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String?
+        }
+    }
+
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // İmza ayarı debug yerine release olarak güncellendi
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
